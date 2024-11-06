cde3754b58ac4c5f3137020fd331474dd98106a1
Hugo Albuquerque <hugo.albuquerque@citi.org.br>
2024-11-06 08:43:34 -0300

    Criei as funções que geram o resultado de cada dado, juntamente com os botões e atribui a função ao resultado, podendo ser constatado no terminal por utilizei o print.

--- main.py
+++ main.py
@@ -4,11 +4,37 @@
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.button import Button
+import random
 
 GUI = Builder.load_file("app.kv")
 
 class MeuAplicativo(App):
     def build(self):
         return GUI
+    
+    def d4(self):
+        resultado = random.randint(1, 4)
+        print(resultado)
+    
+    def d6(self):
+        resultado = random.randint(1, 6)
+        print(resultado)
+        
+    def d8(self):
+        resultado = random.randint(1, 8)
+        print(resultado)
+        
+    def d10(self):
+        resultado = random.randint(1, 10)
+        print(resultado)
+        
+    def d12(self):
+        resultado = random.randint(1, 12)
+        print(resultado)
+        
+    def d20(self):
+        resultado = random.randint(1, 20)
+        print(resultado)
 
 MeuAplicativo().run()
